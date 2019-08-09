60de88c96e8399a3f6b007df6927c04357af9cc3
vivixnc <52637721+vivixnc@users.noreply.github.com>
2019-08-09 11:22:12 -0400

    Update alz.py

--- alz.py
+++ alz.py
@@ -1,12 +1,11 @@
 import pygame
 
+# setting up window
 pygame.init()
-
 screen = pygame.display.set_mode((500,650))
-
 pygame.display.set_caption("Alzhemier's")
-
 clock = pygame.time.Clock()
+act = pygame.event.get()
 
 # inserting image
 #David1 = pygame.image.load('david1.png')
@@ -15,9 +14,9 @@ clock = pygame.time.Clock()
 # Fonts
 font1 = pygame.font.SysFont("Monospace", 30)
 font2 = pygame.font.SysFont("Monospace", 20)
-
+scene = 0.0
 title = font1.render("Alzheimer's", True, (56, 0, 113))
-start = font2.render("press space to continue", True, (56, 0, 113))
+start = font2.render("click anywhere to continue", True, (56, 0, 113))
 
 done = False
 
@@ -28,8 +27,17 @@ while not done:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             done = True
 
-    screen.fill((0, 0, 0))
-    screen.blit(title, ((500 - title.get_width()) // 2, (600 - title.get_height()) // 2))
-    screen.blit(start, ((500 - start.get_width()) // 2, (600 - start.get_height()) // 2 + 30))
-    pygame.display.flip()
-    clock.tick(60)
+    if(scene == 0.0):
+        screen.fill((0, 0, 0))
+        screen.blit(title, ((500 - title.get_width()) // 2, (600 - title.get_height()) // 2))
+        screen.blit(start, ((500 - start.get_width()) // 2, (600 - start.get_height()) // 2 + 30))
+        pygame.display.flip()
+        clock.tick(60)
+
+    if(scene == 0.0 and event.type == pygame.MOUSEBUTTONUP):
+        scene = 0.1
+        screen.fill((0, 0, 0))
+        act1 = font1.render("ACT ONE", True, (255, 255, 255))
+        screen.blit(act1, ((500 - act1.get_width()) // 2, (600 - act1.get_height()) // 2))
+        
+        pygame.display.flip()
